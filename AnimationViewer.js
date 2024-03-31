d1e3ad3d997526b9bd2dcd6cc11f2954ffdef370
BuyMyBeard <95039323+BuyMyBeard@users.noreply.github.com>
2024-03-31 03:25:28 -0400

    This looks nice

---
+++ AnimationViewer.js
@@ -0,0 +1,334 @@
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
+import { EffectComposer } from 'three/addons/postprocessing/EffectComposer.js';
+import { RenderPass } from 'three/addons/postprocessing/RenderPass.js';
+import { UnrealBloomPass } from 'three/addons/postprocessing/UnrealBloomPass.js';
+import { OutputPass } from 'three/addons/postprocessing/OutputPass.js';
+
+export class AnimationViewer {
+    states = [];
+    emotes = [];
+    attacks = [];
+    constructor(containerTag, orbitTargetHeight = .5, backgroundColor = 0xa0a0a0, lightIntensity = 3) {
+        const scene = new THREE.Scene();
+        const camera = new THREE.PerspectiveCamera(75, 4 / 3, 0.1, 100);
+        this.scene = scene;
+        this.camera = camera;
+
+
+        const renderer = new THREE.WebGLRenderer({
+            antialias: true,
+        });
+        this.renderer = renderer;
+        
+        document.querySelector(containerTag).appendChild(renderer.domElement);
+
+
+        const clock = new THREE.Clock();
+        this.clock = clock;
+
+        const actions = {};
+        this.actions = actions;
+
+        renderer.setPixelRatio(window.devicePixelRatio);
+        window.addEventListener("resize", (e) => this.resizeCanvas());
+        this.resizeCanvas();
+        scene.background = new THREE.Color(backgroundColor);
+        scene.fog = new THREE.Fog(backgroundColor, 10, 50);
+
+        const hemiLight = new THREE.HemisphereLight(0xffffff, 0x8d8d8d, lightIntensity);
+        hemiLight.position.set(0, 20, 0);
+        scene.add(hemiLight);
+
+        // ground
+
+        const mesh = new THREE.Mesh(new THREE.PlaneGeometry(100, 100), new THREE.MeshPhongMaterial({ color: 0xcbcbcb, depthWrite: false }));
+        mesh.rotation.x = - Math.PI / 2;
+        mesh.receiveShadow = false;
+        scene.add(mesh);
+
+        const dirLight = new THREE.DirectionalLight(0xffffff, lightIntensity);
+        dirLight.position.set(3, 10, 10);
+        dirLight.castShadow = false;
+        dirLight.shadow.bias = -0.003;
+
+        scene.add(dirLight);
+
+        const controls = new OrbitControls(camera, renderer.domElement);
+        controls.enablePan = false;
+        controls.maxPolarAngle = Math.PI / 2;
+        controls.target.set(0, orbitTargetHeight, 0);
+        controls.minDistance = 2;
+        controls.maxDistance = 15;
+
+        this.controls = controls;
+
+        //controls.update() must be called after any manual changes to the camera's transform
+        camera.position.set(5, 5, 0);
+        controls.update();
+
+        camera.position.z = 5;
+
+        this.animate();
+    }
+    /**
+     * 
+     * @param {string} modelPath 
+     * @param {number} scale 
+     */
+    async loadModel(modelPath, scale = 1) {
+        const loader = new GLTFLoader();
+        let resolvePromise;
+        const promise = new Promise((resolve) => {
+            resolvePromise = resolve;
+        });
+
+        loader.load(
+            modelPath,
+            (gltf) =>
+            {
+                const model = gltf.scene;
+                model.scale.set(scale, scale, scale);
+                this.model = model;
+                this.animations = gltf.animations;
+                model.traverse(function (object)
+                {
+                    if (object.isMesh)
+                    {
+                        object.castShadow = false;
+                        object.receiveShadow = false;
+                        object.material.flatShading = true;
+                        object.material.needsUpdate = true;
+                    }
+
+                });
+                this.scene.add(model);
+
+                this.mixer = new THREE.AnimationMixer(model);
+
+                for (let i = 0; i < this.animations.length; i++) {
+                    const clip = this.animations[i];
+                    const action = this.mixer.clipAction(clip);
+                    this.actions[clip.name] = action;
+                }
+                resolvePromise();
+            },
+            undefined,
+            (error) => console.log(error),
+        );
+        await promise;
+    }
+    /**
+     * 
+     * @param {string} name 
+     * @param {boolean} loop 
+     * @param {number} timescale 
+     * @returns 
+     */
+    addState(name, loop = true, timescale = 1) {
+        const action = this.actions[name];
+        if (!loop) {
+            action.clampWhenFinished = true;
+            action.loop = THREE.LoopOnce;
+        }
+        action.timeScale = timescale;
+        this.states.push(name);
+        return this;
+    }
+    
+    /**
+     * 
+     * @param {string} name 
+     * @param {number} timescale 
+     * @returns 
+     */
+    addEmote(name, timescale = 1) {
+        if (this.actions[name] == undefined) throw(name + " doesn't exist");
+        const action = this.actions[name];
+        action.timeScale = timescale;
+        action.clampWhenFinished = true;
+        action.loop = THREE.LoopOnce;
+        this.emotes.push(name);
+        return this;
+    }
+
+    /**
+     * 
+     * @param {string} name 
+     * @param {number} timescale 
+     * @returns 
+     */
+    addAttack(name, timescale = 1) {
+        if (this.actions[name] == undefined) throw(name + " doesn't exist");
+        const action = this.actions[name];
+        action.timeScale = timescale;
+        action.clampWhenFinished = true;
+        action.loop = THREE.LoopOnce;
+        this.attacks.push(name);
+        return this;
+    }
+
+    createGUI(containerTag, defaultState)
+    {
+        const gui = new GUI({
+            autoPlace: false,
+            width: 300,
+        });
+        const api = {
+            State: defaultState,
+            'Playback Speed': 1,
+
+        };
+        document.querySelector(containerTag).append(gui.domElement);
+
+        // states
+        const statesFolder = gui.addFolder('States');
+
+        const clipCtrl = statesFolder.add(api, 'State').options(this.states);
+
+        clipCtrl.onChange(() => {
+            this.fadeToAction(api.State, 0.5);
+        });
+
+        statesFolder.open();
+
+        // emotes
+        const emoteFolder = gui.addFolder('Actions');
+
+        const createEmoteCallback = function(name) {
+            api[name] = function() {
+                this.fadeToAction(name, 0.2);
+
+                this.mixer.addEventListener('finished', restoreState);
+            }.bind(this);
+
+            emoteFolder.add(api, name);
+        }.bind(this)
+        const restoreStateNoBlending = function() {
+            this.mixer.removeEventListener('finished', restoreStateNoBlending);
+
+            this.fadeToAction(api.State, 0);
+        }.bind(this);
+
+        const restoreState = function() {
+            this.mixer.removeEventListener('finished', restoreState);
+
+            this.fadeToAction(api.State, 0.2);
+        }.bind(this);
+
+        const setUpScatch = function() {
+            const action = this.actions['Scratch'];
+            action.clampWhenFinished = true;
+            action.loop = THREE.LoopOnce;
+            const action2 = this.actions['Sitting'];
+            action2.clampWhenFinished = true;
+            action2.loop = THREE.LoopOnce;
+            api['Scratch'] = () => {
+                this.fadeToAction('Sitting', 0.1);
+                this.mixer.addEventListener('finished', scratch);
+            }
+            const scratch = function() {
+                this.mixer.removeEventListener('finished', scratch);
+                this.mixer.addEventListener('finished', restoreState);
+                this.fadeToAction('Scratch', 0.1);  
+            }.bind(this);
+        }.bind(this);
+
+        if (this.canScratch) {
+            setUpScatch();
+            emoteFolder.add(api, 'Scratch');
+        }
+
+        for (let i = 0; i < this.emotes.length; i++) {
+            createEmoteCallback(this.emotes[i]);
+        }
+
+        if (this.attacks.length > 0) {
+            const createAttackCallback = function(name) {
+                api[name] = function() {
+                    this.fadeToAction(name, 0.2);
+    
+                    this.mixer.addEventListener('finished', restoreState);
+                }.bind(this);
+    
+                attacksFolder.add(api, name);
+            }.bind(this);
+
+            const attacksFolder = gui.addFolder('Attacks');
+
+            for (const attack of this.attacks) {
+                createAttackCallback(attack);
+            }
+            // attacksFolder.close();
+        }
+
+
+        this.activeAction = this.actions[defaultState];
+        this.activeAction.play();
+
+        emoteFolder.open();
+
+        const speedFolder = gui.addFolder('Playback Speed');
+        speedFolder.add(api, 'Playback Speed', 0, 2, 0.01).onChange(this.modifyTimeScale.bind(this));
+        speedFolder.open();
+        
+
+        return this;
+    }
+
+    fadeToAction(name, duration ) {
+
+        this.previousAction = this.activeAction;
+        this.activeAction = this.actions[name];
+
+        if (this.previousAction !== this.activeAction) {
+
+            this.previousAction.fadeOut(duration);
+
+        }
+
+        this.activeAction
+        .reset()
+        .setEffectiveWeight(1)
+        .fadeIn(duration)
+        .play();
+
+    }
+    resizeCanvas() {
+        const width = Math.min(window.innerWidth, 680);
+        const height = width * 3 / 4;
+        this.renderer.setSize(width, height);
+    }
+
+    animate() {
+        const dt = this.clock.getDelta();
+
+        if (this.mixer) this.mixer.update(dt);
+        
+        requestAnimationFrame(this.animate.bind(this));
+        this.controls.update();
+        if (this.composer) this.composer.render();
+        else (this.renderer.render(this.scene, this.camera));
+    }
+    modifyTimeScale(speed) {
+        this.mixer.timeScale = speed;
+    }
+
+    addBloom() {
+        const renderScene = new RenderPass(this.scene, this.camera);
+        const bloomPass = new UnrealBloomPass(new THREE.Vector2(window.innerWidth, window.innerHeight), 1.5, 0.4, 0.85 );
+		bloomPass.threshold = 0;
+		bloomPass.strength = .2;
+		bloomPass.radius = 0;
+
+		const outputPass = new OutputPass();
+
+		this.composer = new EffectComposer(this.renderer);
+		this.composer.addPass(renderScene);
+		this.composer.addPass(bloomPass);
+		this.composer.addPass(outputPass);
+    }
+}
+
--- animations.js
+++ animations.js
@@ -1,200 +1,124 @@
-import * as THREE from 'three';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
+import { AnimationViewer } from './AnimationViewer.js';
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, 4 / 3, 0.1, 1000);
+async function initFoxViewer() {
+    const viewer = new AnimationViewer('#fox-canvas-container');
+    
+    await viewer.loadModel('mesh/fox.glb', 3);
 
-const renderer = new THREE.WebGLRenderer();
-document.querySelector('#fox-canvas-container').appendChild(renderer.domElement);
+    viewer.canScratch = true;
 
-const clock = new THREE.Clock();
-
-let mixer, previousAction, activeAction;
-const actions = {};
-
-renderer.setPixelRatio(window.devicePixelRatio);
-window.addEventListener("resize", (e) => resizeCanvas());
-resizeCanvas();
-scene.background = new THREE.Color(0xa0a0a0);
-scene.fog = new THREE.Fog(0xa0a0a0, 10, 50);
-
-const hemiLight = new THREE.HemisphereLight(0xffffff, 0x8d8d8d, 3);
-hemiLight.position.set(0, 20, 0);
-scene.add(hemiLight);
-
-// ground
-
-const mesh = new THREE.Mesh(new THREE.PlaneGeometry(100, 100), new THREE.MeshPhongMaterial({ color: 0xcbcbcb, depthWrite: false }));
-mesh.rotation.x = - Math.PI / 2;
-mesh.receiveShadow = false;
-scene.add(mesh);
-
-const dirLight = new THREE.DirectionalLight(0xffffff, 3);
-dirLight.position.set(3, 10, 10);
-dirLight.castShadow = false;
-dirLight.shadow.bias = -0.003;
-
-scene.add(dirLight);
-
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.enablePan = false;
-controls.maxPolarAngle = Math.PI / 2;
-controls.target.set(0, .5, 0);
-controls.minDistance = 2;
-controls.maxDistance = 5;
-
-//controls.update() must be called after any manual changes to the camera's transform
-camera.position.set(5, 5, 0);
-controls.update();
-
-const loader = new GLTFLoader();
-loader.load(
-    'mesh/fox.glb',
-    (gltf) =>
-    {
-        const model = gltf.scene;
-        model.traverse(function (object)
-        {
-            if (object.isMesh)
-            {
-                object.castShadow = false;
-                object.receiveShadow = false;
-                object.material.flatShading = true;
-                object.material.needsUpdate = true;
-            }
-
-        });
-        createGUI(model, gltf.animations);
-        scene.add(model);
-    },
-    undefined,
-    (error) => console.log(error),
-);
-
-camera.position.z = 5;
-
-function animate()
-{
-    const dt = clock.getDelta();
-
-	if (mixer) mixer.update(dt);
-
-    requestAnimationFrame(animate);
-    controls.update();
-    renderer.render(scene, camera);
+    viewer.addState('Default')
+    .addState('WalkCycle')
+    .addState('RunCycle', true, 2)
+    .addState('Falling', false)
+    .addEmote('Attack')
+    .addEmote('Wake-Up')
+    .createGUI('#fox-gui-container', 'WalkCycle');
 }
-animate();
 
-function createGUI(model, animations)
-{
-    const states = ['Default', 'WalkCycle', 'RunCycle'];
-    const emotes = ['Attack', 'Wake-Up', 'Scratch'];
-    const gui = new GUI({
-        autoPlace: false,
-        width: 300,
-    });
-    document.querySelector('#fox-gui-container').append(gui.domElement);
-    mixer = new THREE.AnimationMixer(model);
-    const api = { state: 'WalkCycle' };
+async function initHareViewer() {
+    const viewer = new AnimationViewer('#hare-canvas-container');
+    
+    await viewer.loadModel('mesh/white-hare.glb', 8);
 
-    for (let i = 0; i < animations.length; i++)
-    {
-
-        const clip = animations[i];
-        const action = mixer.clipAction(clip);
-        actions[clip.name] = action;
-        if (emotes.indexOf(clip.name) >= 0 || states.indexOf(clip.name) >= 4)
-        {
-            action.clampWhenFinished = true;
-            action.loop = THREE.LoopOnce;
-        }
-
-        switch(clip.name) {
-            case 'RunCycle':
-                action.timeScale = 2;
-                break;
-        }
-    }
-    // states
-
-    const statesFolder = gui.addFolder('States');
-
-    const clipCtrl = statesFolder.add(api, 'state').options(states);
-
-    clipCtrl.onChange(function ()
-    {
-
-        fadeToAction(api.state, 0.5);
-
-    });
-
-    statesFolder.open();
-
-    // emotes
-
-    const emoteFolder = gui.addFolder('Emotes');
-
-    function createEmoteCallback(name)
-    {
-
-        api[name] = function ()
-        {
-
-            fadeToAction(name, 0.2);
-
-            mixer.addEventListener('finished', restoreState);
-
-        };
-
-        emoteFolder.add(api, name);
-
-    }
-
-    function restoreState()
-    {
-
-        mixer.removeEventListener('finished', restoreState);
-
-        fadeToAction(api.state, 0.2);
-
-    }
-
-    for (let i = 0; i < emotes.length; i++)
-    {
-
-        createEmoteCallback(emotes[i]);
-
-    }
-    activeAction = actions[ 'WalkCycle' ];
-    activeAction.play();
-
-
-    emoteFolder.open();
-
+    viewer.addState('Default')
+    .addState('WalkCycle')
+    .addState('RunCycle')
+    .addEmote('Eat')
+    .createGUI('#hare-gui-container', 'WalkCycle');
 }
-function fadeToAction( name, duration ) {
 
-    previousAction = activeAction;
-    activeAction = actions[ name ];
-
-    if ( previousAction !== activeAction ) {
-
-        previousAction.fadeOut( duration );
-
-    }
-
-    activeAction
-    .reset()
-    .setEffectiveWeight( 1 )
-    .fadeIn( duration )
-    .play();
-
+async function initPlayerViewer() {
+    const viewer = new AnimationViewer('#player-canvas-container', 3, 0x222222, .01);
+    
+    await viewer.loadModel('mesh/player.glb', 1.2);
+    
+    viewer.addState('Idle')
+    .addState('Walk')
+    .addState('Walk-Backwards')
+    .addState('Walk-Left')
+    .addState('Walk-Right')
+    .addState('QuickWalk', true, 1.5)
+    .addState('Fall', false)
+    .addState('StartBlocking', false)
+    .addEmote('Drink')
+    .addEmote('Drink-Empty')
+    .addEmote('Interact')
+    .addEmote('Death')
+    .addEmote('Roll')
+    .addEmote('Stagger-Front')
+    .addEmote('Stagger-Back')
+    .addEmote('Stagger-Left')
+    .addEmote('Stagger-Right')
+    .addEmote('WalkThroughFogGate')
+    .addEmote('BlockHit')
+    .addEmote('GuardBreak')
+    .addAttack('LightAttack3')
+    .addAttack('LightAttackCombo')
+    .addAttack('LightAttackCombo2')
+    .addAttack('ThrowSpell')
+    .addAttack('HeavyAttack')
+    .addAttack('HeavyAttackCombo')
+    .createGUI('#player-gui-container', 'Idle',)
+    .addBloom();
 }
-function resizeCanvas()
-{
-    const width = Math.min(window.innerWidth, 800);
-    const height = width * 3 / 4;
-    renderer.setSize(width, height);
+
+async function initEnemyViewer() {
+    const viewer = new AnimationViewer('#enemy-canvas-container', 3, 0x222222, .01);
+    
+    await viewer.loadModel('mesh/enemy.glb', 1.2);
+    
+    viewer.addState('Idle')
+    .addState('Walk')
+    .addState('Walk-Backwards')
+    .addState('Walk-Left')
+    .addState('Walk-Right')
+    .addState('QuickWalk', true, 1.5)
+    .addEmote('Death')
+    .addEmote('Stagger-Front')
+    .addEmote('Stagger-Back')
+    .addEmote('Stagger-Left')
+    .addEmote('Stagger-Right')
+    .addAttack('Attack1')
+    .addAttack('Attack2')
+    .addAttack('Attack2-Boss')
+    .addAttack('Attack3')
+    .addAttack('Attack3-Ex')
+    .addAttack('Attack4')
+    .addAttack('GroundShockwave')
+    .addAttack('MeteorAttackLiftUp')
+    .addAttack('MeteorAttack')
+    .addAttack('AgileEliteAttack')
+    .addAttack('AgileEliteAttack-Ext')
+
+    .createGUI('#enemy-gui-container', 'Idle')
+    .addBloom();
 }
+
+async function initRangedEnemyViewer() {
+    const viewer = new AnimationViewer('#ranger-canvas-container', 3, 0x222222, .01);
+    
+    await viewer.loadModel('mesh/rangedEnemy.glb', 1.2);
+    
+    viewer.addState('Idle')
+    .addState('Walk')
+    .addState('Walk-Backwards')
+    .addState('Walk-Left')
+    .addState('Walk-Right')
+    .addEmote('Death')
+    .addEmote('Stagger-Front')
+    .addEmote('Stagger-Back')
+    .addEmote('Stagger-Left')
+    .addEmote('Stagger-Right')
+    .addAttack('Shoot')
+    .addAttack('RollAttack')
+    .addAttack('GunAttack')
+    .createGUI('#ranger-gui-container', 'Idle')
+    .addBloom();
+}
+
+initFoxViewer();
+initHareViewer();
+initPlayerViewer();
+initEnemyViewer();
+initRangedEnemyViewer();
